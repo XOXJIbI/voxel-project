e7f3c341063f165d10a49d9849fbd31581cb9dc3
Ethereal55 <davidbaymanov@gmail.com>
2024-02-12 17:17:47 +0300

    доработка прыжка
    Добавил два луча по бокам

--- My project/Assets/ClimbingController.cs
+++ My project/Assets/ClimbingController.cs
@@ -4,7 +4,6 @@ using UnityEngine;
 
 public class ClimbingController : MonoBehaviour
 {
-    public float maxObstacleHeight = 1.0f; // ћаксимальна€ высота преп€тстви€, которое персонаж может перепрыгнуть
     public float raycastDistance = 1.0f; // ƒлина луча
     public LayerMask obstacleMask; // —лои объектов, которые €вл€ютс€ преп€тстви€ми
     public float jumpForce;
@@ -16,24 +15,24 @@ public class ClimbingController : MonoBehaviour
     void Update()
     {
         // ќпределите начальную позицию луча (например, нижний конец персонажа)
-        Vector3 raycastOrigin = transform.position - new Vector3(0f, 0.6f, 0f); // Ќапример, смещаем луч вниз от центра персонажа на половину высоты
-    
+        Vector3 raycastOrigin1 = transform.position - new Vector3(0.15f, 0.6f, 0f); // Ќапример, смещаем луч вниз от центра персонажа на половину высоты
+        Vector3 raycastOrigin2 = transform.position - new Vector3(-0.15f, 0.6f, 0f);
+        Vector3 raycastOrigin = transform.position - new Vector3(0f, 0.6f, 0f);
+        Vector3 raycastFake = transform.position;
         // ќпределите направление луча (например, направление взгл€да персонажа или его направление движени€)
         Vector3 raycastDirection = transform.forward; // ѕример: направление вперед от персонажа
-
+        
         // ѕровер€ем наличие преп€тстви€ перед персонажем
         RaycastHit hit;
-        if (Physics.Raycast(raycastOrigin, raycastDirection, out hit, raycastDistance, obstacleMask))
+        if ((Physics.Raycast(raycastOrigin1, raycastDirection, out hit, raycastDistance, obstacleMask) || 
+            Physics.Raycast(raycastOrigin2, raycastDirection, out hit, raycastDistance, obstacleMask) ||
+            Physics.Raycast(raycastOrigin, raycastDirection, out hit, raycastDistance, obstacleMask)) &&
+            !Physics.Raycast(raycastFake, raycastDirection, out hit, raycastDistance, obstacleMask))
         {
-            // ≈сли луч столкнулс€ с преп€тствием, провер€ем его высоту
-            float obstacleHeight = hit.point.y - transform.position.y; // ¬ысота преп€тстви€ относительно текущей позиции персонажа
-            if (rb.velocity != Vector3.zero)
+            if (rb.velocity.x != 0 && rb.velocity.z != 0 && rb.velocity.y == 0)
             {
-                if (obstacleHeight <= maxObstacleHeight)
-                {
-                    // ≈сли высота преп€тстви€ меньше или равна максимальной высоте, выполн€ем прыжок
-                    Jump();
-                }
+                // ≈сли высота преп€тстви€ меньше или равна максимальной высоте, выполн€ем прыжок
+                Jump();
             }
         }
     }
