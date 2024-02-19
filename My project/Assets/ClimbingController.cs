2eceb81e4451009cd92f9230d8bdeef59f3c7b4a
Ethereal55 <davidbaymanov@gmail.com>
2024-02-19 21:42:23 +0300

    Сделал ограничение на прыжок

--- My project/Assets/ClimbingController.cs
+++ My project/Assets/ClimbingController.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class ClimbingController : MonoBehaviour
@@ -12,22 +13,31 @@ public class ClimbingController : MonoBehaviour
     {
         rb = GetComponent<Rigidbody>();
     }
-    void Update()
+    void FixedUpdate()
     {
+        bool canJump = false;
         // ќпределите начальную позицию луча (например, нижний конец персонажа)
         Vector3 raycastOrigin = transform.position - new Vector3(0f, 0.6f, 0f);
-        Vector3 raycastFake = transform.position;
+        Vector3 raycastFake = transform.position + new Vector3(0f, 0.3f, 0f);
         // ќпределите направление луча (например, направление взгл€да персонажа или его направление движени€)
         Vector3 raycastDirection = transform.forward; // ѕример: направление вперед от персонажа
         Quaternion rotationQuaternion = Quaternion.Euler(0, 40, 0);
         Vector3 rotatedVector = rotationQuaternion * raycastDirection;
         // ѕровер€ем наличие преп€тстви€ перед персонажем
         RaycastHit hit;
-        if (Physics.Raycast(raycastOrigin, raycastDirection, out hit, raycastDistance, obstacleMask)|| 
-            Physics.Raycast(raycastOrigin, rotatedVector, out hit, raycastDistance, obstacleMask)||
+        if (Physics.Raycast(raycastOrigin, raycastDirection, out hit, raycastDistance, obstacleMask) || 
+            Physics.Raycast(raycastOrigin, rotatedVector, out hit, raycastDistance, obstacleMask) ||
             Physics.Raycast(raycastOrigin, -rotatedVector, out hit, raycastDistance, obstacleMask))
         {
-            if (rb.velocity.x != 0 && rb.velocity.z != 0 && rb.velocity.y == 0)
+        
+            if (Physics.Raycast(raycastFake, raycastDirection, out hit, raycastDistance, obstacleMask) ||
+            Physics.Raycast(raycastFake, rotatedVector, out hit, raycastDistance, obstacleMask) ||
+            Physics.Raycast(raycastFake, -rotatedVector, out hit, raycastDistance, obstacleMask))
+            {
+                canJump = true;
+            }
+
+            if (!canJump && (Mathf.Abs(rb.velocity.x) > 0.1f || Mathf.Abs(rb.velocity.z) > 0.1f) && rb.velocity.y == 0)
             {
                 // ≈сли высота преп€тстви€ меньше или равна максимальной высоте, выполн€ем прыжок
                 Jump();
